41866a8137d16bcd7f32c86825f657c7f9b8c26c
Diestro Corleone <matias.perez.mcd@gmail.com>
2022-11-13 12:51:00 -0300

    Update blogModel.js

--- back/models/blogModel.js
+++ back/models/blogModel.js
@@ -1,4 +1,4 @@
-import db from './database/db.js'; // Importamos la conexión.
+import db from '../database/db.js'; // Importamos la conexión.
 import {DataTypes} from 'sequelize'; // Método desde sequelize.
 
 //Indicamos los tipos de dato de los campos en nuestra tabla. Primer parámetro es el nombre de nuestra tabla, luego la columna y su tipo de dato. No hacen falta el ID ni la creación o update.
