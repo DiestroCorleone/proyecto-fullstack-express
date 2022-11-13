2e74d1739616397d6c4acdf89b475b206c6f2e51
Diestro Corleone <matias.perez.mcd@gmail.com>
2022-11-13 12:15:59 -0300

    Update app.js
    Added try/catch to check database connection.

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import blogRoutes from "./routes/blogRoutes.js"; // Se recomienda incluir la extensión del archivo.
+import db from './database/db.js';
 
 const app = express(); // Separamos todos los métodos de express para su posterior uso.
 const port = 8000;
@@ -9,6 +10,14 @@ app.use("/blogs", blogRoutes);
 
 app.use(cors());
 app.use(express.json()); // Ambas son configuraciones que hacen que la información que viaje, la modela y configura.
+
+// Conexión a base de datos, verificamos si funcionó.
+try {
+  await db.authenticate();
+  console.log("Conexión exitosa");
+} catch(error){
+  console.log("Error de conexión: " + error);
+}
 
 app.listen(port, () => {
   console.log("Example app listening at http://localhost:" + port);
