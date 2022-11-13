9ca7b4e3d77a5ab9d3b21aad00d521458dd33dac
Diestro Corleone <matias.perez.mcd@gmail.com>
2022-11-13 12:28:10 -0300

    Create blogModel.js
    Created blogModel.

---
+++ back/model/blogModel.js
@@ -0,0 +1,11 @@
+import db from './database/db.js'; // Importamos la conexión.
+import {DataTypes} from 'sequelize'; // Método desde sequelize.
+
+//Indicamos los tipos de dato de los campos en nuestra tabla. Primer parámetro es el nombre de nuestra tabla, luego la columna y su tipo de dato. No hacen falta el ID ni la creación o update.
+const blogModel = db.define('blogs',{
+  title: {type: DataTypes.STRING},
+  content: {type: DataTypes.STRING}
+});
+
+export default blogModel;
+
