16504472a3e1d01585b2c968cb83392414bb0d82
DiestroCorp <matias.perez.mcd@gmail.com>
2022-11-15 15:14:03 -0300

    Added controllers for getBlog and deleteBlog.

--- back/controllers/blogController.js
+++ back/controllers/blogController.js
@@ -10,3 +10,29 @@ export const getAllBlogs = async (req, res) => {
     res.json({ message: error.message }); // De fallar, devolvemos un JSON con el error.
   }
 };
+
+export const getBlog = async (req, res) => {
+  try {
+    const blog = await blogModel.findOne({ where: { id: req.params.id } }); // Pasamos como parámetro el ID en la URL.
+
+    if (blog === null) {
+      console.log(`Blog con id ${req.params.id} no encontrado`);
+    } else {
+      res.json(blog);
+    }
+  } catch (e) {
+    res.json({ message: error.message });
+  }
+};
+
+export const deleteBlog = async (req, res) => {
+  try {
+    const blog = await blogModel
+      .destroy({ where: { id: req.params.id } })
+      .then((res) =>
+        console.log(`Blog con id ${req.params.id} eliminado correctamente.`)
+      );
+  } catch (e) {
+    res.json({ message: error.message });
+  }
+};
--- back/routes/blogRoutes.js
+++ back/routes/blogRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blogController.js";
+import {
+  getAllBlogs,
+  getBlog,
+  deleteBlog,
+} from "../controllers/blogController.js";
 
 const router = express.Router(); // Guardamos todos los métodos de Router (GET, POST, PUT, DELETE).
 
 router.get("/", getAllBlogs); // La barra sola es el index.
-// router.get('/:id', getBlog); // Agregamos el parámetro del request que tomará el ID del blog.
+router.get("/:id", getBlog); // Agregamos el parámetro del request que tomará el ID del blog.
 // router.post('/', createBlog);
 // router.put('/:id', updateBlog);
-// router.delete(':id', deleteBlog);
+router.delete("/delete/:id", deleteBlog);
 
 export default router;
